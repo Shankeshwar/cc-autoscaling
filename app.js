c4c151afd218e452a30976185d1a3d9834ebf6eb
Shankeshwar <138300378+Shankeshwar@users.noreply.github.com>
2025-05-10 07:28:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send('<h1>EC2 Auto Scaling demo v1.0.0</h1>');
 })
 
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
-})
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on http://0.0.0.0:${port}`);
+});
+
